505a527fa0f7b65e123ac4b66cacb55ed9fbb7e7
lucsimao <lucasimao64@gmail.com>
2022-02-05 12:58:43 -0300

    feat: update App function as class

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import './App.css';
 
+import { Component } from 'react';
 import { FormularioCadastro } from './components/FormularioCadastro';
 import { ListaDeNotas } from './components/ListaDeNotas';
 
-function App() {
-  return (
-    <section>
-      <FormularioCadastro />
-      <ListaDeNotas />
-    </section>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <section>
+        <FormularioCadastro />
+        <ListaDeNotas />
+      </section>
+    );
+  }
 }
-
-export default App;
