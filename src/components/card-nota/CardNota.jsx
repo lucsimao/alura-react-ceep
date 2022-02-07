9def0847ae0dd9bcdaf008811bf24a7c09d9240c
lucsimao <lucasimao64@gmail.com>
2022-02-07 19:37:54 -0300

    feat: add delete.svg

--- src/components/card-nota/CardNota.jsx
+++ src/components/card-nota/CardNota.jsx
@@ -1,5 +1,7 @@
-import { Component } from 'react';
 import './style.css';
+
+import { Component } from 'react';
+import { ReactComponent as DeleteSVG } from '../../assets/img/delete.svg';
 
 export class CardNota extends Component {
   render() {
@@ -7,6 +9,7 @@ export class CardNota extends Component {
       <section className="card-nota">
         <header className="card-nota_header">
           <h3 className="card-nota_title">{this.props.titulo}</h3>
+          <DeleteSVG />
         </header>
         <p className="card-nota_text">{this.props.texto}</p>
       </section>
